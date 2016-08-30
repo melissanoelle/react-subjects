3f7d3bd72c9c701599a7c2f46e48cf5ba63ecc4b
Melissa Noelle <melissamatara@gmail.com>
2016-08-30 16:50:51 -0400

    Completed second exercise

--- subjects/Components/exercise.js
+++ subjects/Components/exercise.js
@@ -16,6 +16,12 @@ import { render } from 'react-dom'
 
 const styles = {}
 
+const DATA = [
+  { id: 1, name: 'USA', description: 'Land of the Free, Home of the brave' },
+  { id: 2, name: 'Brazil', description: 'Sunshine, beaches, and Carnival' },
+  { id: 3, name: 'Russia', description: 'World Cup 2018!' }
+]
+
 styles.tab = {
   display: 'inline-block',
   padding: 10,
@@ -34,40 +40,60 @@ styles.panel = {
   padding: 10
 }
 
-const Tabs = React.createClass({
-  render() {
+class Tab extends React.Component {
+  render () {
+    let tabStyle
+    this.props.name === this.props.active ? tabStyle = styles.activeTab : tabStyle = styles.tab
     return (
-      <div className="Tabs">
-        <div className="Tab" style={styles.activeTab}>
-          Active
-        </div>
-        <div className="Tab" style={styles.tab}>
-          Inactive
-        </div>
-        <div className="TabPanel" style={styles.panel}>
-          Panel
-        </div>
+      <div className="Tab" style={tabStyle} data-name={this.props.name} onClick={this.props.onClick}>
+        {this.props.name}
       </div>
     )
   }
-})
+}
 
-const App = React.createClass({
-  render() {
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      activeTab: 'USA'
+    }
+    this.handleTabClick = this.handleTabClick.bind(this)
+    this.getDescription = this.getDescription.bind(this)
+  }
+
+  handleTabClick (e) {
+    window.hi = e.target
+    this.setState({activeTab: e.target.dataset.name})
+  }
+
+  getDescription (stuff) {
+    return stuff.map((thing) => { if (thing.name === this.state.activeTab) return thing.description })
+  }
+
+  render () {
+    let isActive = true
+    let tabs = this.props.countries.map((country) => {
+      return ( <Tab 
+        key={country.id} 
+        name={country.name} 
+        active={this.state.activeTab} 
+        onClick={this.handleTabClick} /> )
+      isActive = false
+    })
     return (
       <div>
         <h1>Countries</h1>
-        <Tabs data={this.props.countries}/>
+        <div className="Tabs">
+          {tabs}
+        </div>
+        <div className="TabPanel" style={styles.panel}>
+          {this.getDescription(this.props.countries)}
+        </div>
       </div>
     )
   }
-})
-
-const DATA = [
-  { id: 1, name: 'USA', description: 'Land of the Free, Home of the brave' },
-  { id: 2, name: 'Brazil', description: 'Sunshine, beaches, and Carnival' },
-  { id: 3, name: 'Russia', description: 'World Cup 2018!' }
-]
+}
 
 render(<App countries={DATA}/>, document.getElementById('app'), function () {
   require('./tests').run(this)
