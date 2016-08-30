73853180dbce39fd93009c314fed0bdc10dab8d7
Melissa Noelle <melissamatara@gmail.com>
2016-08-30 18:43:58 -0400

    Working on exercise 4, but timeboxed and display bugs are annoying

--- subjects/ImperativeToDeclarative/exercise.js
+++ subjects/ImperativeToDeclarative/exercise.js
@@ -9,16 +9,11 @@ import { render, findDOMNode } from 'react-dom'
 import $ from 'jquery'
 import 'bootstrap-webpack'
 
-const Modal = React.createClass({
-
-  propTypes: {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.node
-  },
-
+class Modal extends React.Component {
   render() {
+    let className = this.props.display ? 'show-shit' : 'hide-shit'
     return (
-      <div className="modal fade">
+      <div className={`modal fade ${className}`}>
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
@@ -32,44 +27,48 @@ const Modal = React.createClass({
       </div>
     )
   }
+}
+Modal.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  children: React.PropTypes.node,
+  display: React.PropTypes.bool.isRequired
+}
 
-})
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      display: true
+    }
+    this.toggleModal = this.toggleModal.bind(this)
+  }
 
-const App = React.createClass({
-
-  openModal() {
-    $(findDOMNode(this.refs.modal)).modal('show')
-  },
-
-  closeModal() {
-    $(findDOMNode(this.refs.modal)).modal('hide')
-  },
+  toggleModal() {
+    this.setState({display: !this.state.display})
+  }
 
   render() {
     return (
       <div className="container">
         <h1>Let’s make bootstrap modal declarative</h1>
-
         <button
           className="btn btn-primary"
-          onClick={this.openModal}
+          onClick={this.toggleModal}
         >open modal</button>
 
-        <Modal ref="modal" title="Declarative is better">
+        <Modal ref="modal" title="Declarative is better" display={this.state.display}>
           <p>Calling methods on instances is a FLOW not a STOCK!</p>
           <p>It’s the dynamic process, not the static program in text space.</p>
           <p>You have to experience it over time, rather than in snapshots of state.</p>
           <button
-            onClick={this.closeModal}
+            onClick={this.toggleModal}
             type="button"
             className="btn btn-default"
           >Close</button>
         </Modal>
-
       </div>
     )
   }
-
-})
+}
 
 render(<App/>, document.getElementById('app'))
