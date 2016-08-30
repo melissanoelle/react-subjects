41890209e84588afb5522daa4e14dabd5e16701a
Melissa Noelle <melissamatara@gmail.com>
2016-08-30 14:27:52 -0400

    Finish exercise 1

--- subjects/Rendering/exercise.js
+++ subjects/Rendering/exercise.js
@@ -28,14 +28,54 @@ const DATA = {
   ]
 }
 
-function Menu() {
-  return (
-    <div>
-      Open the console, you have failing tests.
-    </div>
-  )
+class Menu extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      filter: 'mexican',
+      sort: 'name'
+    }
+    this.filterChange = this.filterChange.bind(this)
+    this.buttonChange = this.buttonChange.bind(this)
+  }
+
+  filterChange (e) {
+    this.setState({filter: e.target.value}) 
+  }
+
+  buttonChange (e) {
+    let sorts = {
+      'name': { filter: '-name', label: 'DESC' },
+      '-name': { filter: 'name', label: 'ASC' }
+    }
+    let sort = sorts[this.state.sort]
+    this.setState({sort: sort.filter})
+    e.target.value = sort.label
+    e.target.innerHTML = sort.label
+  }
+
+  render () {
+   var foodStuffs = DATA.items.sort(sortBy(this.state.sort))
+      .filter((thing) => { return thing.type === this.state.filter })
+      .map((burrito) => { 
+      return ( <li key={burrito.id}>{burrito.name}</li> )
+    })
+
+    return (
+      <div>
+        <h1>{DATA.title}</h1>
+        <select value={this.props.filter} onChange={this.filterChange}>
+          <option value="mexican">Mexican</option>
+          <option value="english">English</option>
+        </select>
+        <button onClick={this.buttonChange} value="ASC">ASC</button>
+        <ul>
+          {foodStuffs}
+        </ul>
+      </div>
+    )
+  }
 }
 
-render(<Menu/>, document.getElementById('app'), () => {
-  require('./tests').run()
-})
+render(<Menu />, document.getElementById('app'))
+
