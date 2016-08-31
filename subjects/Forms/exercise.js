de0d857749816f1f9274ff3a86413ef8856403ce
Melissa Noelle <melissamatara@gmail.com>
2016-08-31 13:54:09 -0400

    Completed forms exercise

--- subjects/Forms/exercise.js
+++ subjects/Forms/exercise.js
@@ -20,7 +20,51 @@ import React from 'react'
 import { render } from 'react-dom'
 import serializeForm from 'form-serialize'
 
-const CheckoutForm = React.createClass({
+class CheckoutForm extends React.Component { 
+  constructor(props) {
+    super(props)
+    this.state = {
+      isSameAsBilling: false,
+      billingName: '',
+      billingState: '',
+      shippingName: '',
+      shippingState: '',
+      billingStateError: false,
+      shippingStateError: false
+    }
+    this.handleSameAsBilling = this.handleSameAsBilling.bind(this)
+  }
+
+  handleSameAsBilling () {
+    this.setState({ isSameAsBilling: !this.state.isSameAsBilling })
+  }
+
+  handleFormChange (field, e) {
+    let newState = {}
+
+    if (field.indexOf('shipping') >= 0) {
+      newState.isSameAsBilling = false
+      newState.shippingState = this.billingState
+      newState.shippingName = this.billingName
+    }
+
+    newState[field] = e.target.value
+
+    if (field.indexOf('State') >= 0) {
+      newState[field + 'Error'] = e.target.value.length > 2
+    }
+
+    this.setState(newState)
+  }
+
+  getShippingName () {
+    return this.state.isSameAsBilling ? this.state.billingName : this.state.shippingName
+  }
+  
+  getShippingState () {
+    return this.state.isSameAsBilling ? this.state.billingState : this.state.shippingState
+  }
+
   render() {
     return (
       <div>
@@ -29,29 +73,45 @@ const CheckoutForm = React.createClass({
           <fieldset>
             <legend>Billing Address</legend>
             <p>
-              <label>Billing Name: <input type="text"/></label>
+              <label>Billing Name: <input 
+                type="text" 
+                value={this.state.billingName} 
+                onChange={this.handleFormChange.bind(this, 'billingName')} /></label>
             </p>
             <p>
-              <label>Billing State: <input type="text" size="2"/></label>
+              <label>Billing State: <input 
+                type="text" 
+                size="2"
+                value={this.state.billingState} 
+                onChange={this.handleFormChange.bind(this, 'billingState')} /></label>
             </p>
+            <div className="error" style={{display: `${this.state.billingStateError ? 'block' : 'none'}`}}>State must be a two-letter abbreviation.</div>
           </fieldset>
 
           <br/>
 
           <fieldset>
-            <label><input type="checkbox"/> Same as billing</label>
+            <label><input type="checkbox" checked={this.state.isSameAsBilling} onChange={this.handleSameAsBilling}/> Same as billing</label>
             <legend>Shipping Address</legend>
             <p>
-              <label>Shipping Name: <input type="text"/></label>
+              <label>Shipping Name: <input 
+                type="text" 
+                value={this.getShippingName()} 
+                onChange={this.handleFormChange.bind(this, 'shippingName')} /></label>
             </p>
             <p>
-              <label>Shipping State: <input type="text" size="2"/></label>
+              <label>Shipping State: <input 
+                type="text" 
+                size="2" 
+                value={this.getShippingState()} 
+                onChange={this.handleFormChange.bind(this, 'shippingState')} /></label>
             </p>
+            <div className="error" style={{display: `${this.state.shippingStateError ? 'block' : 'none'}`}}>State must be a two-letter abbreviation.</div>
           </fieldset>
         </form>
       </div>
     )
   }
-})
+}
 
 render(<CheckoutForm/>, document.getElementById('app'))
